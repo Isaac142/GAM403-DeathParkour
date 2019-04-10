d470cb76cb0ef9ac4d70aa94e39cd7b8f99ea097
Isaac142 <embramaze@gmail.com>
2019-04-10 22:57:40 +1000

    walk jump

---
+++ DeathParkour/Assets/Scripts/ChangeWeapons.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWeapons : MonoBehaviour
+{
+    public GameObject[] weapons;
+    PlayerMovement movement;
+    public GameObject knife;
+    public GameObject gun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Debug.Log(weapons.Length);
+        WeaponSwap(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            WeaponSwap(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            WeaponSwap(1);
+        }
+
+        if (gun.gameObject.CompareTag("Knife"))
+        {
+            movement.normalSpeed = movement.normalSpeed;
+        }
+    }
+
+    void WeaponSwap(int weaponNumber)
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            if (i != weaponNumber)
+                weapons[i].SetActive(false);
+            else
+                weapons[i].SetActive(true);
+
+        }
+    }
+}
---
+++ DeathParkour/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    CharacterController controller;
+    public float verticalVelocity;
+    public float gravity = 14.0f;
+    public float jumpForce = 10.0f;
+    public float normalSpeed = 1.0f;
+    public float gunspeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerJumping();
+    }
+
+    void PlayerJumping()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+        controller.Move(moveVector * Time.deltaTime);
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(h, 0, v) * normalSpeed);
+    }
+}
