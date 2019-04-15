5aeae6dab21fe030e7942f1e4ffafd8f4c68fa31
Isaac142 <embramaze@gmail.com>
2019-04-15 12:00:29 +1000

    respawn

---
+++ DeathParkour/Assets/Scripts/Respawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = respawnPoint.transform.position;
+    }
+}
