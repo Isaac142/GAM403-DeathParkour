698555970a7460eaf0156f12aa261494005e2344
Isaac142 <embramaze@gmail.com>
2019-05-02 14:34:02 +1000

    title screen

---
+++ DeathParkour/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float timer = 0f;
+    public int health = 100;
+    public bool isPaused = false;
+    public UIManager UI;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        if (!isPaused && timer >= 0)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            UI.Pause(isPaused);
+        }
+        
+    }
+}
---
+++ DeathParkour/Assets/Scripts/UIManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public bool isTitleScreen;
+    public GameObject pausePanel;
+    public GameObject inGamePanel;
+    public GameManager GM;
+
+    [Header("In Game UI")]
+    public TextMeshProUGUI timerText;
+    public Image healthBar;
+    public GameObject winText;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(!isTitleScreen)
+        {
+            timerText.text = GM.timer.ToString("F2");
+
+            healthBar.fillAmount = GM.health / 100f;
+        }
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Pause(bool pause)
+    {
+        
+        if(pausePanel != null)
+        {
+            pausePanel.SetActive(pause);
+            inGamePanel.SetActive(!pause);
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.gameObject.CompareTag("Win"))
+    //    {
+    //        Debug.Log("You Win");
+    //    }
+    //}
+}
